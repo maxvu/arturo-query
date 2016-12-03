3c46696cce065a55019fa88796427d1ddbb0f513
Max Vu <maxwell.vu@gmail.com>
2016-12-03 01:31:49 -0500

    More parsing tests

--- test/00-parsing.js
+++ test/00-parsing.js
@@ -1,3 +1,4 @@
+'use strict';
 var assert = require( 'assert' );
 var parse = require( '../src/arturo-query.js' ).parse;
 var parse_safe = require( '../src/arturo-query.js' ).parse_safe;
@@ -17,18 +18,6 @@ describe( 'parser', function () {
             assert.equal(
                 parse( '     hello world     ' ).getCanonical(),
                 parse( 'hello world' ).getCanonical()
-            );
-        } );
-        
-        it( 'should be negated by all three operators', function () {
-            assert.equal(
-                parse( '-smoking' ).getCanonical(),
-                parse( '!smoking' ).getCanonical(),
-                parse( 'not smoking' ).getCanonical()
-            );
-            assert.notEqual(
-                parse( 'smoking' ).getCanonical(),
-                parse( '!smoking' ).getCanonical()
             );
         } );
         
@@ -83,8 +72,14 @@ describe( 'parser', function () {
         
         it( 'should not escape', function () {
             assert.throws( () => {
-                parse( " \" " );
-            } );
+                parse( " \\\" " );
+            }, Error );
+        } );
+        
+        it( 'throw when unmatched', function () {
+            assert.throws( () => {
+                parse( " ' " );
+            }, Error );
         } );
         
     } );
@@ -93,8 +88,122 @@ describe( 'parser', function () {
         
         it( 'nest', function () {
             assert.equal(
-                parse( '()((()())())()' ).getCanonical(),
-                parse( '' ).getCanonical()
+                parse( '()((()(a))())()' ).getCanonical(),
+                parse( 'a' ).getCanonical()
+            );
+        } );
+        
+        it( 'ignore whitespace', function () {
+            assert.equal(
+                parse( ' ()(    (()(a    ))())   (   )   ' ).getCanonical(),
+                parse( '    a' ).getCanonical()
+            );
+        } );
+        
+    } );
+    
+    describe( 'or expressions', function () {
+        
+        it( 'are recognized', function () {
+            assert.equal(
+                parse( 'a or b' ).getCanonical(),
+                parse( 'a | b' ).getCanonical()
+            );
+            assert.notEqual(
+                parse( 'a or b' ).getCanonical(),
+                parse( 'a b' ).getCanonical()
+            );
+        } );
+        
+        it( 'die when seen at beginning of stream', function () {
+            assert.throws( () => {
+                parse( "or a b" );
+            }, Error );
+            assert.throws( () => {
+                parse( "| a b" );
+            }, Error );
+        } );
+        
+        it( 'die when seen at end of stream', function () {
+            assert.throws( () => {
+                parse( "a b or" );
+            }, Error );
+            assert.throws( () => {
+                parse( "a b |" );
+            }, Error );
+        } );
+        
+        it( 'die when seen twice in a row', function () {
+            assert.throws( () => {
+                parse( "a or or b" );
+            }, Error );
+            assert.throws( () => {
+                parse( "a || b" );
+            }, Error );
+            assert.throws( () => {
+                parse( "a | | b" );
+            }, Error );
+        } );
+        
+        it( 'are not clobbered by barewords beginning with "or"', function () {
+            assert.equal(
+                parse( 'oration' ).getCanonical(),
+                parse( '"oration"' ).getCanonical()
+            );
+        } );
+        
+    } );
+    
+    describe( 'negated expressions', function () {
+        
+        it( 'are recognized', function () {
+            let a = '-alpha';
+            let b = 'not alpha';
+            let c = '!alpha';
+            assert.equal(
+                parse( a ).getCanonical(),
+                parse( b ).getCanonical(),
+                parse( c ).getCanonical()
+            );
+        } );
+        
+        it( 'negate bare words', function () {
+            assert.equal(
+                parse( '-smoking' ).getCanonical(),
+                parse( '!smoking' ).getCanonical(),
+                parse( 'not smoking' ).getCanonical()
+            );
+            assert.notEqual(
+                parse( 'smoking' ).getCanonical(),
+                parse( '!smoking' ).getCanonical()
+            );
+        } );
+        
+        it( 'negate expressions', function () {
+            assert.equal(
+                parse( '-(hooliganism)' ).getCanonical(),
+                parse( '!(hooliganism)' ).getCanonical(),
+                parse( 'not (hooliganism)' ).getCanonical()
+            );
+            assert.notEqual(
+                parse( 'tomfoolery' ).getCanonical(),
+                parse( '!tomfoolery' ).getCanonical()
+            );
+        } );
+        
+        it( 'toggle', function () {
+            assert.equal(
+                parse( '--toggle' ).getCanonical(),
+                parse( '- -toggle' ).getCanonical(),
+                parse( 'not not toggle' ).getCanonical(),
+                parse( '!!toggle' ).getCanonical()
+            );
+        } );
+        
+        it( 'are not clobbered by barewords beginning with "not"', function () {
+            assert.equal(
+                parse( 'nothing' ).getCanonical(),
+                parse( '"nothing"' ).getCanonical()
             );
         } );
         
