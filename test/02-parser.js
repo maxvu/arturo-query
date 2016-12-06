ffbdea62bc904cf5a454e34e199b8b03d88e8f59
Max Vu <maxwell.vu@gmail.com>
2016-12-05 20:20:30 -0500

    parser tests

--- test/02-parser.js
+++ test/02-parser.js
@@ -8,7 +8,7 @@ var conj = expr.conj;
 var disj = expr.disj;
 
 let parse = ( query ) => {
-    return (new parser( query )).debug().parse().reduce();
+    return (new parser( query )).parse().reduce();
 };
 
 describe( 'parser', function () {
@@ -84,7 +84,137 @@ describe( 'parser', function () {
             ) );
         } );
         
-        it( 'should not confuse terms that begin with "or"', function () {
+    } );
+    
+    describe( 'quoted terms', function () {
+        
+        it( 'should be ignored when empty', function () {
+            parse( '""' ).should.deepEqual( parse( '' ) );
+        } );
+        
+        // TODO: quoted terms with just whitespace inside
+        
+        it( 'should throw when unterminated', function () {
+            ( () => { return parse( '"' ) } ).should.throw();
+            ( () => { return parse( '\'' ) } ).should.throw();
+        } );
+        
+        it( 'should escape quoting character', function () {
+            ( () => {
+                return parse( ` "esc\\\"aped" ` )
+            } )().should.not.throw().and.deepEqual(
+                parse( ` 'esc\"aped' ` )
+            );
+        } );
+    
+    } );
+    
+    describe( 'quoted terms', function () {
+        
+        it( 'should be ignored when empty', function () {
+            parse( '""' ).should.deepEqual( parse( '' ) );
+        } );
+        
+        // TODO: quoted terms with just whitespace inside
+        
+        it( 'should throw when unterminated', function () {
+            ( () => { return parse( '"' ) } ).should.throw();
+            ( () => { return parse( '\'' ) } ).should.throw();
+        } );
+        
+        it( 'should escape quoting character', function () {
+            ( () => {
+                return parse( ` "esc\\\"aped" ` )
+            } )().should.not.throw().and.deepEqual(
+                parse( ` 'esc\"aped' ` )
+            );
+        } );
+        
+        it( 'should not be interrupted by parentheticals', function () {
+            ( () => {
+                return parse( ` 'elk(gull)' ` );
+            } )()._children.should.containEql(
+                new term( "elk(gull)" )
+            )
+        } );
+    
+    } );
+    
+    describe( 'negated expressions', function () {
+        
+        it( 'should not be confused with terms that begin with "not"', function () {
+            parse(
+                ' nothing '
+            ).should.deepEqual( parse(
+                ' "nothing" '
+            ) );
+        } );
+
+        it( 'should negate bare terms', function () {
+            parse(
+                ' !slug '
+            )._children.should.containEql(
+                new term( 'slug' ).negate()
+            );
+        } );
+        
+        it( 'should negate quoted terms', function () {
+            parse(
+                ' !"quail" '
+            )._children.should.containEql(
+                new term( 'quail' ).negate()
+            );
+            parse(
+                " !'tiger' "
+            )._children.should.containEql(
+                new term( 'tiger' ).negate()
+            );
+        } );
+        
+        it( 'should negate paren groups', function () {
+            parse(
+                ' !( worm ) '
+            )._children.should.containEql(
+                new term( 'worm' ).negate()
+            );
+            parse(
+                ' !( -( worm ) ) '
+            )._children.should.containEql(
+                new term( 'worm' )
+            );
+            parse(
+                ' !( not ( !worm ) ) '
+            )._children.should.containEql(
+                new term( 'worm' ).negate()
+            );
+        } );
+        
+        it( 'can occur in combination', function () {
+            parse(
+                ' !not -( worm ) '
+            )._children.should.containEql(
+                new term( 'worm' ).negate()
+            );
+            
+            parse(
+                ' not not not panda '
+            )._children.should.containEql(
+                new term( 'panda' ).negate()
+            );
+            
+            parse(
+                ' !! starfish '
+            )._children.should.containEql(
+                new term( 'starfish' )
+            );
+        } );
+        
+        
+    } );
+    
+    describe( 'disjunctive operators', function () {
+        
+        it( 'should not be confused with terms that begin with "or"', function () {
             parse(
                 ' orca '
             ).should.deepEqual( parse(
@@ -92,14 +222,46 @@ describe( 'parser', function () {
             ) );
         } );
         
-        it( 'should not confuse terms that begin with "and"', function () {
-            parse(
-                ' andean-cat '
-            ).should.deepEqual( parse(
-                ' "andean-cat" '
-            ) );
+        it( 'cannot occur at the beginning of a query', function () {
+            ( () => {
+                parse( ' or termite ' )
+            } ).should.throw();
+            
+            ( () => {
+                parse( ' | termite ' )
+            } ).should.throw();
+        } );
+        
+        it( 'cannot occur at the end of a query', function () {
+            ( () => {
+                parse( ' toad or ' )
+            } ).should.throw();
+            
+            ( () => {
+                parse( ' toad | ' )
+            } ).should.throw();
+        } );
+        
+        it( 'cannot occur twice and adjacently', function () {
+            ( () => {
+                parse( ' tarantula or or squirrel ' )
+            } ).should.throw();
+            
+            ( () => {
+                parse( ' sparrow | or snail ' )
+            } ).should.throw();
+            
+            ( () => {
+                parse( ' yak || vulture ' )
+            } ).should.throw();
+            
+            ( () => {
+                parse( ' yak | \t| vulture ' )
+            } ).should.throw();
         } );
         
     } );
     
 } );
+
+
